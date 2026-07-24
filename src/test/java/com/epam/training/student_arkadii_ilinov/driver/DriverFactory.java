eeb01ecedfc43be52094dd9e4b4c432741b74386
Arkdadii Ilinov <arkadiil@proton.me>
2026-07-24 22:04:21 +0200

    chore: remove unused import in DriverFactory

--- src/test/java/com/epam/training/student_arkadii_ilinov/driver/DriverFactory.java
+++ src/test/java/com/epam/training/student_arkadii_ilinov/driver/DriverFactory.java
@@ -1,7 +1,6 @@
 package com.epam.training.student_arkadii_ilinov.driver;
 
 import com.epam.training.student_arkadii_ilinov.utils.ConfigReader;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
